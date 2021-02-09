d14a99db4617b240e0a0008e05c9a27c9db451eb
vbopardi <vbopard@gmail.com>
2021-02-09 11:46:11 -0800

    Fixed Fixme

--- Fixme.py
+++ Fixme.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-'''
-Your solution for each function should be only a single line long.
-In particular, you may not use any loops when implementing these functions;
-instead, you must use either the map and filter functions or list comprehensions.
-'''
+
 
 def evens(n):
     '''
@@ -21,10 +17,13 @@ def evens(n):
     []
     '''
 
+    return [x for x in range(n + 1) if x % 2 == 0]
+
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n
+    inclusive that contain the digit 3.
 
     >>> threes(2)
     []
@@ -38,10 +37,13 @@ def threes(n):
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
 
+    return [x for x in range(n + 1) if '3' in str(x)]
+
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 4 characters long.
+    Returns a list of all words in the input
+    text that are less than 4 characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -55,7 +57,8 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    return [ word for word in text.split() if len(word)<=4 ]
+
+    return [word for word in text.split() if len(word) <= 4]
 
 
 def squares(n):
@@ -73,10 +76,13 @@ def squares(n):
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
 
+    return [n**2 for n in range(1, n + 1)]
+
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of the lengths
+    of the corresponding strings.
 
     >>> lengths([])
     []
@@ -85,3 +91,5 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+
+    return [len(word) for word in strings]
